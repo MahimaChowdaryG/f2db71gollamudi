c0cd528d7c23e06ef7eb0d5475cd9015212df477
MahimaChowdaryG <gollamudimahimachowdary7@gmail.com>
2022-11-16 11:49:11 -0600

    bonus

--- routes/animals.js
+++ routes/animals.js
@@ -2,7 +2,7 @@ var express = require('express');
 const animal_controlers= require('../controllers/animals'); 
 var router = express.Router(); 
  
-/* GET costumes */ 
+/* GET animals */ 
 router.get('/', animal_controlers.animal_view_all_Page ); 
 
 /* GET detail animal page */ 
