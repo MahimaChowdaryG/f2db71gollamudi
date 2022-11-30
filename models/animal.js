80d2c3e0b788b37c66099712bfcf16e958f165fc
MahimaChowdaryG <gollamudimahimachowdary7@gmail.com>
2022-11-29 19:18:38 -0600

    bonus

--- models/animal.js
+++ models/animal.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose") 
 const animalSchema = mongoose.Schema({ 
  name: String, 
- lifetime: Number, 
- color: String 
+ lifetime: { type: Number, min: 1, max: 1000 }, 
+ color: { type: String, minLength : 2}  
 }) 
  
 module.exports = mongoose.model("Animal", 
